3126209ea352eddea4ca3aa7ef19ac4ed2e330e7
23564 <707280427@qq.com>
2020-12-23 17:27:16 +0800

    @babel/polyfill

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # webpack4-demo-20201222
+
+
+## [babel 转码Promise](https://blog.csdn.net/purple_lumpy/article/details/88573549?utm_medium=distribute.pc_relevant_t0.none-task-blog-BlogCommendFromMachineLearnPai2-1.control&depth_1-utm_source=distribute.pc_relevant_t0.none-task-blog-BlogCommendFromMachineLearnPai2-1.control)
+
+```
+npm install --save-dev @babel/polyfill
+//然后呢，我们需要在所有代码运行前，引入 polyfill , 它会补充我们缺少的内容
+```
+
+### promise 转码会增加很多代码用来实现promise对象。
--- src/index.js
+++ src/index.js
@@ -1,7 +1,23 @@
-// eslint-disable-next-line func-style
-var a = ()=>{
-  console.log('122')
-}
+import "@babel/polyfill";
 
-console.log('222:',a)
+
+// eslint-disable-next-line func-style
+const a = (num)=>new Promise((resolve,reject)=>{
+    if (num>10) {
+      resolve(num)
+    } else {
+      // eslint-disable-next-line prefer-promise-reject-errors
+      reject('错误')
+    }
+  })
+
+a(20).then(data=>{
+  console.log('111:',data)
+})
+
+a(2).then(data=>{
+  console.log('222:',data)
+})
+
+
 
